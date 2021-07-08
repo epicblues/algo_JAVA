768d4e5ce48fe7ad633080db025e74aa8bc52b06
KMS\user <epicblue@hanmail.net>
2021-07-08 19:18:39 +0900

    algorithm start learned finding same char in string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,26 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         StringBuilder sentence=new StringBuilder();
         String[] words = {"ya ","yao ","yasuo"};
 
         for (String w : words) sentence.append(w);
         System.out.println(sentence.toString());
+        String input = new String();
+        System.out.println("문장을 입력하시오");
+        input = scan.next();
+        quizWord quizW = new quizWord(input);
+        quizW.findSameChar();
+        quizW.isUniqueChars();
 
-        String word;
-        word = "abacedsafsdcfedsf";
-        char[] wordArray = word.toCharArray();
-        for (char w : wordArray) System.out.println(w);
-
+        int val;
+        val = 'c' - 'a';
+        System.out.println(val);
     }
 }
---
+++ src/quiz2/Main.java
@@ -0,0 +1,4 @@
+package quiz2;
+
+public class Main {
+}
