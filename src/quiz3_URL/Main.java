dd13248dda6cdd6e850eba77b67d526496d9c0e9
KMS\user <epicblue@hanmail.net>
2021-07-09 21:56:28 +0900

    solved quiz3 that find char and change it to longer character we count target and make new array for extra space

--- src/quiz3_URL/Main.java
+++ src/quiz3_URL/Main.java
@@ -6,12 +6,50 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Type strings to URL");
         Scanner scan = new Scanner(System.in);
-        String input = scan.next();
-        Char[] inputChar = input.toCharArray();
+        String input = scan.nextLine();
+        char[] inputChar = input.toCharArray();
         char blank = ' ';
-        int blankCount = 0;
-        for (int i =0; i<inputChar.length;i++) {
-            if (char(inputChar[i]) == blank)
+        int spaceCount = 0;
+        for (int i = 0; i < inputChar.length; i++) {
+            if (inputChar[i] == blank) {
+                spaceCount++;
+            }
+        }
+        char[] bigCharArray = new char[inputChar.length + 2 * spaceCount];
+        int j = 0;
+        for (int i = 0; i < inputChar.length; i++) {
+            if (inputChar[i] == blank) {
+                bigCharArray[j] = '%';
+                bigCharArray[j + 1] = '2';
+                bigCharArray[j + 2] = '0';
+                j += 3;
+            } else {
+                bigCharArray[j] = inputChar[i];
+                j++;
+            }
+        }
+        System.out.println(bigCharArray);
+
+    }
+    void replaceSpaces(char[] str,int trueLength) {
+        int spaceCount = 0; int index =0; int i=0;
+        for (i=0; i<trueLength; i ++) {
+            if (str[i] == ' ') {
+                spaceCount++;
+            }
+        }
+        index = trueLength + spaceCount*2;
+        if(trueLength < str.length) str[trueLength] = '\0';
+        for(i=trueLength-1; i>=0;i--) {
+            if(str[i] == ' ') {
+                str[index -1] = '0';
+                str[index -2] = '2';
+                str[index -3] = '%';
+                index = index - 3;
+            } else {
+                str [index -1] = str[i];
+                index--;
+            }
         }
     }
 }
