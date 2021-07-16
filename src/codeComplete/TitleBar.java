8bff84e60323d90ef2a4465d0db6e4b874f97f23
epicblues <epicblue@hanmail.net>
2021-07-16 13:57:52 +0900

    save sample

---
+++ src/codeComplete/TitleBar.java
@@ -0,0 +1,20 @@
+package codeComplete;
+
+import javax.swing.*;
+
+public class TitleBar {
+    int color;
+    private static final int  COLOR_BLUE = 0xFF;
+    private static final int TITLE_BAR_COLOR = COLOR_BLUE;
+    public TitleBar() {
+        this.color = 0x00;
+
+    }
+
+    public void setColorBlue() {
+        this.color = TITLE_BAR_COLOR;
+    }
+
+
+
+}
