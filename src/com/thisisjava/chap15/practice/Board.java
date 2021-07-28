da993030c0e6029406774a8537c8e8df9ab73ee0
김민성 <epicblue@hanmail.net>
2021-07-28 17:43:44 +0900

    chap 15 practice7 finish

---
+++ src/com/thisisjava/chap15/practice/Board.java
@@ -0,0 +1,30 @@
+package com.thisisjava.chap15.practice;
+
+public class Board {
+	private String title;
+	private String content;
+	
+	public Board(String title, String content) {
+		super();
+		this.setTitle(title);
+		this.setContent(content);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
