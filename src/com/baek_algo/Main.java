4a1694bd87d3513a0aa0f4dd68ad547c8914b9b1
김민성 <epicblue@hanmail.net>
2021-08-03 19:59:03 +0900

    chap 18 network server/client start

--- src/com/baek_algo/Main.java
+++ src/com/baek_algo/Main.java
@@ -1,27 +1,25 @@
 package com.baek_algo;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.io.*;
 
 public class Main{
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int inputNum = Integer.parseInt(br.readLine());
-        int count = 0;
-        for (int j=0; j<inputNum;j++) {
-            HashSet<Character> set = new HashSet<>();
-            String inputStr = br.readLine();
-            if (inputStr.length() == 1) break;
-            for(int i =0; i<inputStr.length();i++) {
-                if(!(set.add(Character.valueOf(inputStr.charAt(i))))) {
-                    count++;
-                    break;
-                }
-            }
-        }
-         System.out.println(count);
-        }
-      
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> intArray = new ArrayList<>();
+        for(int i = num1; i<= num2; i ++) {
+            intArray.add(i);
+        } 
         
         
+        
+        		
+        		
+        		
+        	
     }
+}
