677934504061a794a0eb1d0dcdbdb0b94de3a34e
KMS <epicblue@hanmail.net>
2021-08-04 21:40:18 +0900

    finished prelearn thisisjava

--- src/com/thisisjava/chap12/threadpool/CallbackExample.java
+++ src/com/thisisjava/chap12/threadpool/CallbackExample.java
@@ -1,57 +1,58 @@
-package com.thisisjava.chap12.threadpool;
-
-import java.nio.channels.CompletionHandler;
-import java.util.concurrent.*;
-
-public class CallbackExample {
-	private ExecutorService executorService;
-
-	public CallbackExample() {
-		executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-	}
-
-	private CompletionHandler<Integer, Void> callback = new CompletionHandler<>() {
-
-		@Override
-		public void completed(Integer result, Void attachment) {
-			System.out.println("completed() 실행 : " + result);
-
-		}
-
-		@Override
-		public void failed(Throwable exc, Void attachment) {
-			System.err.println("failed() 실행 : " + exc.getMessage());
-
-		}
-
-	};
-
-	public void doWork(final String x, final String y) {
-		Runnable task = new Runnable() {
-
-			public void run() {
-				try {
-					int intX = Integer.parseInt(x);
-					int intY = Integer.parseInt(y);
-					int result = intX + intY;
-					callback.completed(result, null);
-				} catch (NumberFormatException e) {
-					callback.failed(e, null);
-				}
-
-			}
-
-		};
-
-		executorService.submit(task);
-	}
-
-	public static void main(String[] args) {
-		CallbackExample example = new CallbackExample();
-		example.doWork("3", "3");
-		example.doWork("삼", "6");
-		example.executorService.shutdown();
-		
-	}
-
-}
+//package com.thisisjava.chap12.threadpool;
+//
+//
+//import java.nio.channels.CompletionHandler;
+//import java.util.concurrent.*;
+//
+//public class CallbackExample {
+//	private ExecutorService executorService;
+//
+//	public CallbackExample() {
+//		executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//	}
+//
+//	private CompletionHandler<Integer, Void> callback = new CompletionHandler<>() {
+//
+//		@Override
+//		public void completed(Integer result, Void attachment) {
+//			System.out.println("completed() 실행 : " + result);
+//
+//		}
+//
+//		@Override
+//		public void failed(Throwable exc, Void attachment) {
+//			System.err.println("failed() 실행 : " + exc.getMessage());
+//
+//		}
+//
+//	};
+//
+//	public void doWork(final String x, final String y) {
+//		Runnable task = new Runnable() {
+//
+//			public void run() {
+//				try {
+//					int intX = Integer.parseInt(x);
+//					int intY = Integer.parseInt(y);
+//					int result = intX + intY;
+//					callback.completed(result, null);
+//				} catch (NumberFormatException e) {
+//					callback.failed(e, null);
+//				}
+//
+//			}
+//
+//		};
+//
+//		executorService.submit(task);
+//	}
+//
+//	public static void main(String[] args) {
+//		CallbackExample example = new CallbackExample();
+//		example.doWork("3", "3");
+//		example.doWork("삼", "6");
+//		example.executorService.shutdown();
+//
+//	}
+//
+//}
--- src/com/thisisjava/chap18/network/ServerExample.java
+++ src/com/thisisjava/chap18/network/ServerExample.java
@@ -1,9 +1,12 @@
 package com.thisisjava.chap18.network;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class ServerExample {
 
@@ -17,10 +20,26 @@ public class ServerExample {
 				Socket socket = serverSocket.accept();
 				InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
 				System.out.println("연결 수락함 " + isa.getHostName());
-			} 
+
+		
+		byte [] bytes = null;
+		String message = null;
+		InputStream is = socket.getInputStream();
+		bytes = new byte[100];
+		int readByteCount = is.read(bytes);
+		message = new String(bytes,0,readByteCount,"UTF-8");
+		System.out.println("데이터 받기 성공 " + message );
+
+		OutputStream os = socket.getOutputStream();
+		message = "hello client";
+		bytes = message.getBytes(StandardCharsets.UTF_8);
+		os.write(bytes);
+		os.flush();
+		System.out.println("[데이터 보내기 성공]");
+
+			}
 		} catch(Exception e) {}
-		
-		
+
 		if(!serverSocket.isClosed()) {
 			try {
 				serverSocket.close();
