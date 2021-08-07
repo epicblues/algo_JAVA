6b5e31fd7a3f5d1566873d9f0f09cf2a4f4630d1
KMS <epicblue@hanmail.net>
2021-08-07 16:13:46 +0900

    error fix

--- src/com/baek_algo/Main.java
+++ src/com/baek_algo/Main.java
@@ -6,33 +6,7 @@ import java.io.*;
 
 public class Main{
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int num1 = Integer.parseInt(st.nextToken());
-        int num2 = Integer.parseInt(st.nextToken());
-        
-        double sum = 0;
-        
-        for(int i = num1; i<= num2; i ++) {
-        	int temp = i;
-            for(int n=9; n>=0; n--) {
-            	double num;
-            	if ( Math.pow(10, n)> temp) {} 
-            	else {
-            	num = Math.floor(temp / Math.pow(10, n));
-            	temp = temp - num * Math.pow(10, n);
-            	sum += (num >=1 ? num : 0);
-            	}
-            }
-        } 
-        
-        System.out.println((int)sum);
-        
-        
-        
-        		
-        		
-        		
+      
         	
     }
 }
